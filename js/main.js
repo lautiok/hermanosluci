16a48fd61768cd8fa3f5b58839e79f9cb2263c94
Nahuel Guerra <nahuelguerra56b@gmail.com>
2023-08-08 16:17:59 -0300

    pax

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
-var button = document.querySelector('.button');
-var nav = document.querySelector('.nav');
+const button = document.querySelector('.button');
+const nav = document.querySelector('.nav');
 button.addEventListener('click', function () {
     nav.classList.toggle('activo');
 });
+
+var slider = document.querySelector(".slider");
+
+function nextSlide() {
+  var currentSlide = slider.querySelector(".slide.active");
+  var nextSlide = currentSlide.nextElementSibling;
+
+  if (nextSlide) {
+    currentSlide.classList.remove("active");
+    nextSlide.classList.add("active");
+  } else {
+    currentSlide.classList.remove("active");
+    slider.querySelector(".slide:first-child").classList.add("active");
+  }
+}
+
+setInterval(nextSlide, 5000);
